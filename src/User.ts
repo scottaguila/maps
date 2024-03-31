6cda4c4bb4ae667cc874fe60ea90e826916981ee
scottaguila <scott.aguila@gmail.com>
2024-03-31 08:45:49 -0400

    Added 'impements Mappable" to Company and User classes

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
+import { Mappable } from './Map';
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
